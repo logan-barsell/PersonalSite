8b4824b5a4514da80bd9c5295b7a50f1f6a4434d
Logan Barsell <loganbarsell@Logans-MBP.lan1>
2019-03-20 16:53:44 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express'),
+	
+	http = require('http')
+
+	url = require('url')
+
+	path = require('path')
+	// Creates app from the library
+	app = express (),
+	// Creates a session middleware for making cookie settings
+	session = require('express-session'),
+	// enables fs module for file operations
+	fs = require('fs'),
+	// extracts data from request stream and exposes it on req.body
+	bodyParser = require('body-parser'),
+	// Enables hashing of passwords
+	passwordHash = require('password-hash'),
+	// Accesses Postgres server
+	pg = require('pg'),
+
+
+	app.set('views', __dirname+'/public')
+	// Sets view engine to pug
+	// app.set('view engine', 'pug')
+	app.use('/', express.static(path.join(
+		__dirname+"/public")))
+
+
+	.use(session({
+	  secret: 'secure as fuck',
+	  saveUninitialized: false,
+	  resave: false,
+	  cookie: {
+	  	secure: false,
+	  	maxAge: 1000 * 60 * 60 * 60 * 60
+	   }
+	}))
+
+	// Middleware that only parses urlencoded bodies
+	.use(bodyParser.urlencoded({extended:true}))
+
+	.use(express.json())
+	.use(express.urlencoded({extended:true}))
+
+	
+	// Serves static files
+	.use('/static', express.static(__dirname+"/static"))
+
+	.get('/', (req, res) => {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+		res.render('index')
+	})
+
+
+http.createServer(app).listen(8080, 'localhost')
